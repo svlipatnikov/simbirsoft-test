f09bc356d03a06a2944a484ddf767c1fa72280a5
SeregyLipatnikov <sv.lipatnikov@gmail.com>
2020-12-28 22:25:54 +0400

    getdata to text

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,20 +1,39 @@
 import React, { useState, useEffect } from 'react'
+import './content.css'
 
 export default function Content() {
-  const url = 'https://jsonplaceholder.typicode.com/users'
+  // const url = 'https://jsonplaceholder.typicode.com/users'
+  const url = 'http://api.football-data.org/v2/matches'
 
-  const [data, setData] = useState({})
+  function sendRequest(method, url) {
+    return fetch(url, {
+      headers: { 'X-Auth-Token': '8c4f30d4f4354979ac043901839c7664' },
+      dataType: 'json',
+      type: 'GET',
+    }).then((response) => {
+      if (response.ok) {
+        return response.json()
+      }
+
+      return response.json().then((error) => {
+        const e = new Error('Что-то пошло не так')
+        e.data = console.error
+        throw e
+      })
+    })
+  }
+
+  const [jsonData, setJsonData] = useState({})
 
   useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((inputData) => {
-        setData(inputData)
-        console.log(typeof inputData)
-        console.log(inputData)
-        console.log(JSON.stringify(inputData, null, ' '))
+    sendRequest('Get', url)
+      .then((data) => {
+        console.log(typeof data)
+        console.log(data)
+        setJsonData(data)
       })
+      .catch((err) => console.log(err))
   }, [])
 
-  return <div>{JSON.stringify(data[0], null, ' ')}</div>
+  return <pre className="json-text">{JSON.stringify(jsonData, null, ' ')}</pre>
 }
