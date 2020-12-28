46d77732278238fd6ef46a9c073a688b92d99479
SeregyLipatnikov <sv.lipatnikov@gmail.com>
2020-12-28 06:43:44 +0400

    first

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <div>Leagues</div>
+      <div>Teams</div>
+      <div>League calendar</div>
+      <div>Team calendar</div>
+    </div>
+  )
+}
